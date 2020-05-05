557b13f763f32f8e0a61c33206c22a64ef35af02
Ryan Duff <ryanduff613@gmail.com>
2020-05-05 16:23:03 -0700

    add new spec, acheive meaningful failure

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -7,4 +7,8 @@ describe("Scrabble#word_score") do
     expect(Scrabble.new("a").word_score()).to(eq(1))
   end
 
+  it('returns a score equal to the length of a multiletter word composed of aeioulnrs or t ')do
+    expect(Scrabble.new("lein").word_score()).to(eq(4))
+  end
+  
 end
