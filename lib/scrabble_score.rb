f0a5e3af687bcda1adee3914bd32f6044abc2275
Ryan Duff <ryanduff613@gmail.com>
2020-05-05 23:20:59 -0700

    code not working, attempting to split entry into array, then loop through the array while also looping through array of one point words to return 1 point for every letter

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -3,17 +3,18 @@ require('pry')
 class Scrabbleword
   def initialize(word)
     @word = word
-    @word_letters = @word.split()
+    @word_letters = @word.split(//)
+    puts @word_letters
+    @one_point_letters = [a,e,i,o,u,l,n,r,s,t]
     @score = 0
   end
 
   def word_score
     @word_letters.each do |word_letter|
-      if word_letter == "a" 
-        @score += 1
-      elsif word_letter == "e"
-        @score += 1
-      else
+      @one_point_letters.each do |one_point_letter|
+        if word_letter == one_point_letter 
+          @score += 1
+        end
       end
     end
     @score
